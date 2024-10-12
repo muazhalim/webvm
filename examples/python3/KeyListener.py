3d27dec2e69d7410e17a7418d6bb8789daffdaff
muazhalim <87516254+muazhalim@users.noreply.github.com>
2024-10-12 18:31:30 +0100

    Update KeyListener.py

--- examples/python3/KeyListener.py
+++ examples/python3/KeyListener.py
@@ -1,41 +1,40 @@
-import msvcrt
+import curses
 from threading import Thread
 from Direction import Direction
 
 class KeyListener(Thread):
-	"""Thread class that gets input from the keyboard's keys."""
+    """Thread class that gets input from the keyboard's keys."""
 
-	def __init__(self):
-		# The current direction
-		self.direction = None
-		# Boolean indicating whether this game is paused
-		self.paused = False
-		# Boolean indicating whether to quit the game
-		self.quit = False
-		# Boolean indicating whether this thread is active
-		self.active = False
-		# Call to the superclass' constructor
-		super(KeyListener, self).__init__()
+    def __init__(self, stdscr):
+        # The current direction
+        self.direction = None
+        # Boolean indicating whether this game is paused
+        self.paused = False
+        # Boolean indicating whether to quit the game
+        self.quit = False
+        # Boolean indicating whether this thread is active
+        self.active = False
+        # Store the stdscr (screen object) for handling curses input
+        self.stdscr = stdscr
+        # Call to the superclass' constructor
+        super(KeyListener, self).__init__()
 
-	def run(self):
-		self.active = True
+    def run(self):
+        self.active = True
 
-		while self.active:
-			key = msvcrt.getwch()
+        while self.active:
+            key = self.stdscr.getch()
 
-			# Get a direction
-			if key == "\xe0":
-				key = msvcrt.getwch()
+            # Get a direction based on key input
+            if key in Direction.DIRECTIONS:
+                self.direction = key
+            # Flip the pause toggle
+            elif key == ord(" "):
+                self.paused = not self.paused
+            # Flip the quit toggle
+            elif key == ord("q"):
+                self.quit = True
 
-				if key in Direction.DIRECTIONS:
-					self.direction = key
-			# Flip the pause toggle
-			elif key == " ":
-				self.paused = not self.paused
-			# Flip the quit toggle
-			elif key == "q":
-				self.quit = True
-
-	def end(self):
-		"""End this thread."""
-		self.active = False
+    def end(self):
+        """End this thread."""
+        self.active = False
