ede0ab37c3beca47780451225a10e78c4f2d181b
muazhalim <87516254+muazhalim@users.noreply.github.com>
2024-10-12 18:32:21 +0100

    Update Direction.py

--- examples/python3/Direction.py
+++ examples/python3/Direction.py
@@ -1,20 +1,25 @@
+import curses
+
 class Direction:
-	"""This class represents a directions on a plane."""
+    """This class represents directions on a plane."""
 
-	UP = "H"
-	LEFT = "K"
-	DOWN = "P"
-	RIGHT = "M"
-	DIRECTIONS = ["H", "K", "P", "M"]
+    # Using curses key codes for arrow keys
+    UP = curses.KEY_UP
+    LEFT = curses.KEY_LEFT
+    DOWN = curses.KEY_DOWN
+    RIGHT = curses.KEY_RIGHT
 
-	@staticmethod
-	def opposite(direction):
-		"""Return the opposite of the current direction."""
-		if direction == Direction.UP:
-			return Direction.DOWN
-		elif direction == Direction.LEFT:
-			return Direction.RIGHT
-		elif direction == Direction.DOWN:
-			return Direction.UP
-		elif direction == Direction.RIGHT:
-			return Direction.LEFT
+    # List of all directions
+    DIRECTIONS = [UP, LEFT, DOWN, RIGHT]
+
+    @staticmethod
+    def opposite(direction):
+        """Return the opposite of the current direction."""
+        if direction == Direction.UP:
+            return Direction.DOWN
+        elif direction == Direction.LEFT:
+            return Direction.RIGHT
+        elif direction == Direction.DOWN:
+            return Direction.UP
+        elif direction == Direction.RIGHT:
+            return Direction.LEFT
